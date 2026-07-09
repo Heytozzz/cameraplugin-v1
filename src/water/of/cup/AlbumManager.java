7d31fbd0dfcc8be9fa8504e6110f38953b721c90
Heytoz <heytoz.2003@gmail.com>
2026-07-09 02:43:14 -0600

    Fix missing imports (Map, Block, Entity) in AlbumManager.java

--- src/water/of/cup/AlbumManager.java
+++ src/water/of/cup/AlbumManager.java
@@ -6,14 +6,17 @@ import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.block.Biome;
+import org.bukkit.block.Block;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
